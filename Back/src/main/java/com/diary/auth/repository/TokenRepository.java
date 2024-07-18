69c2ca547de761bea4312293b2559872376eb0a5
Yunseong Jeong <jyerd333@gmail.com>
2024-07-18 17:17:08 +0900

    fix: Long > String

--- Back/src/main/java/com/diary/auth/repository/TokenRepository.java
+++ Back/src/main/java/com/diary/auth/repository/TokenRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface TokenRepository extends JpaRepository<Token, Long> {
+public interface TokenRepository extends JpaRepository<Token, String> {
     Token findByRefreshToken(String refreshToken);
     Optional<Token> findByUser(User user);
 }
