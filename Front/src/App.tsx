b6d1451aeb7b8fd0559fbff1e85f3e2e89cbc7dd
Yunseong Jeong <jyerd333@gmail.com>
2024-07-19 20:20:47 +0900

    feat: Add routes

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -1,10 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignUp from './components/auth/SignUp';
+import SignIn from './components/auth/SignIn';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        
+        <Route path='/' element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
       </Routes>
     </Router>
   )
